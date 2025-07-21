ae2f2a1a1cc6a6d9b7489923253beb517549c91a
ManxlPC <notmanxl2169@gmail.com>
2025-07-21 20:17:54 +0530

    added database and alot of dependencies

---
+++ database/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectToDb = async() =>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("database has been successfully connected")
+    } catch (e) {
+        console.error(e);
+        process.exit(1)
+        
+    }
+
+}
+
+module.exports = connectToDb
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const connectToDb = require('./database/db')
+
+
+
+connectToDb();
 
 const app = express();
 const PORT = 3000;
-
-server.listen(PORT, ()=>{
-    console.log("Server is now running")
-    
-})
 
 
 
 const server = http.createServer(app);
 const io = socketIo(server);
+
+server.listen(PORT, ()=>{
+    console.log("Server is now running")
+    
+})
