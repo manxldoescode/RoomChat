43fb0ffa46b366b1565b8f2c44c4cca7df17bd60
ManalLAP <notmanxl2169@gmail.com>
2025-07-22 12:03:23 +0530

    Added authentication

--- server.js
+++ server.js
@@ -2,21 +2,27 @@ require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const authRoutes = require('./routes/auth-routes')
 const connectToDb = require('./database/db')
 
 
 
 connectToDb();
 
+
+
 const app = express();
 const PORT = 3000;
 
 
 
-const server = http.createServer(app);
-const io = socketIo(server);
+// const io = socketIo(server);
 
-server.listen(PORT, ()=>{
-    console.log("Server is now running")
-    
+
+app.use(express.json())
+
+
+app.listen(PORT, ()=> {
+    console.log(`server is running on ${PORT}`) 
 })
+app.use('/auth', authRoutes)
